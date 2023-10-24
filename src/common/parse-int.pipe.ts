ce52a81910dab455428276e0c163591314473323
brayan.orellana@cu.ucsg.edu.ec <brayan.orellana@cu.ucsg.edu.ec>
2023-10-24 11:36:25 -0500

    initial code

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -11,7 +11,7 @@ export class ParseIntPipe implements PipeTransform {
     const val = parseInt(value, 10);
     if (isNaN(val)) {
       throw new BadRequestException(`${value} is not an number`);
-    }
+    } 
     return val;
   }
 }
